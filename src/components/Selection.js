c0212f6bc805128fde9da281c45a366a51aea965
KrishnaPatelRb <93961739+KrishnaPatelRb@users.noreply.github.com>
2022-02-17 12:57:20 +0530

    1234

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
  
 export default function Selection(props) {
-    const [color,setColor]=useState("red")
-    return (
-        <div className="fix-box" style={{background:props.applyColor}} onClick={()=>{
+    const [color,setColor]=useState({background:""})
     
+    return (
+        
+        <div className="fix-box" style={color} onClick={()=>{
+            setColor(props.a)
         }}>
             <h2 style={{}} className="subeading">Selection</h2>
         </div>
