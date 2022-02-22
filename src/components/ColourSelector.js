e1d8c59b10361fe8a4ee1442bc3350ab7f0d4ce5
KrishnaPatelRb <93961739+KrishnaPatelRb@users.noreply.github.com>
2022-02-22 10:16:24 +0530

    1234

--- src/components/ColourSelector.js
+++ src/components/ColourSelector.js
@@ -4,7 +4,7 @@ const ColourSelector = (props) => {
   const { config, selectNextBackground } = props
   const { background } = config;
   return (
-    <button className={props.config.className} onClick={() => selectNextBackground({background: background})}>
+    <button style={{background}} className={props.config.className} onClick={() => selectNextBackground({background: background})}>
       {props.config.label}
     </button>
   )
